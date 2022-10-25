1064ad902d87ce53867ed489e66c3754f6e86a28
zcq98 <1229896069@qq.com>
2022-10-25 21:59:07 +0000

    setup

---
+++ tna_mpls-bfsketch/bfrt_python/setup.py
@@ -0,0 +1,102 @@
+from ipaddress import ip_address
+import sys
+import time
+import json
+import signal
+sys.path.append('/usr/local/lib/python3.5/dist-packages')
+import redis
+import schedule
+
+p4 = bfrt.main.pipe
+redis_cli = redis.Redis(host='localhost',port=6379,db=15)
+
+# This function can clear all the tables and later on other fixed objects
+# once bfrt support is added.
+def clear_all(verbose=True, batching=True):
+    global p4
+    global bfrt
+    
+    def _clear(table, verbose=False, batching=False):
+        if verbose:
+            print("Clearing table {:<40} ... ".
+                  format(table['full_name']), end='', flush=True)
+        try:    
+            entries = table['node'].get(regex=True, print_ents=False)
+            try:
+                if batching:
+                    bfrt.batch_begin()
+                for entry in entries:
+                    entry.remove()
+            except Exception as e:
+                print("Problem clearing table {}: {}".format(
+                    table['name'], e.sts))
+            finally:
+                if batching:
+                    bfrt.batch_end()
+        except Exception as e:
+            if e.sts == 6:
+                if verbose:
+                    print('(Empty) ', end='')
+        finally:
+            if verbose:
+                print('Done')
+
+        # Optionally reset the default action, but not all tables
+        # have that
+        try:
+            table['node'].reset_default()
+        except:
+            pass
+    
+    # The order is important. We do want to clear from the top, i.e.
+    # delete objects that use other objects, e.g. table entries use
+    # selector groups and selector groups use action profile members
+    
+
+    # Clear Match Tables
+    for table in p4.info(return_info=True, print_info=False):
+        if table['type'] in ['MATCH_DIRECT', 'MATCH_INDIRECT_SELECTOR']:
+            _clear(table, verbose=verbose, batching=batching)
+
+    # Clear Selectors
+    for table in p4.info(return_info=True, print_info=False):
+        if table['type'] in ['SELECTOR']:
+            _clear(table, verbose=verbose, batching=batching)
+            
+    # Clear Action Profiles
+    for table in p4.info(return_info=True, print_info=False):
+        if table['type'] in ['ACTION_PROFILE']:
+            _clear(table, verbose=verbose, batching=batching)
+    
+# Route Set
+def simple_fwd():
+    p4.Ingress.simple_fwd.clear()
+    p4.Ingress.simple_fwd.add_with_hit(ingress_port=130,port=131)
+    p4.Ingress.simple_fwd.add_with_hit(ingress_port=131,port=130)
+
+# Set Redis and Clear Sketch and Bloomfilter
+def set_redis():
+    global time
+    global redis_cli
+    print("--- 5 SECONDS ---")
+    t = time.perf_counter()
+    for table in p4.Egress.bf_sketch.info(return_info=True, print_info=False):
+        if table['type'] in ['REGISTER']:
+            key = table['full_name'].split('.')[3]
+            value = table['node'].dump(json=True,return_ents=True,from_hw=True)
+            redis_cli.set(key, value)
+    print("Sketch获取耗时: ")
+    print(time.perf_counter() - t)
+    for table in p4.Egress.bf_sketch.info(return_info=True, print_info=False):
+        if table['type'] in ['REGISTER']:
+            table['node'].clear()
+
+simple_fwd()
+schedule.every(5).seconds.do(set_redis)
+time.sleep(5)
+schedule.run_pending()
+# while True:
+#     try:
+#         schedule.run_pending()
+#     except:
+#         break
